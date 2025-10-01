2ec2576532e2b9f77efb2c51f2561ad31620fd71
CodieMisiani <nimrodmisiani1@gmail.com>
2025-10-01 17:44:21 +0300

    fix: reorder components in Content for improved structure

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -1,4 +1,5 @@
 import Footer from "../footer/Footer";
+import About from "./About/About";
 import ExperienceList from "./Experiences/ExperienceList";
 import ProjectList from "./Projects/ProjectList";
 import SkillList from "./Skills/SkillsList";
@@ -6,12 +7,13 @@ import SkillList from "./Skills/SkillsList";
 const Content = () => {
   return (
     <div className="static p-[24px] sm:p-[32px] md:max-w-[536px] lg:max-w-[736px] xl:min-w-[736px] md:absolute md:right-0 lg:py-[88px] lg:pr-[88px] md:pl-0">
-        <ProjectList />
-        <ExperienceList />
-        <SkillList />
-        <Footer />
+      <About />
+      <ProjectList />
+      <ExperienceList />
+      <SkillList />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Content
+export default Content;
