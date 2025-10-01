5105fd4ade725ee09b05502468b5c8c58e12793d
CodieMisiani <nimrodmisiani1@gmail.com>
2025-10-01 17:44:53 +0300

    feat: add About component with personal introduction and background

---
+++ src/components/content/About/About.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <div className="leading-loose">
+        <p className="mt-[32px]">
+          I’m a creative problem solver who’s passionate about building
+          meaningful, user-centered digital experiences.I love taking on
+          challenges that involve complex problem-solving and transforming them
+          into efficient, elegant code. For me, development is more than just
+          writing code—it's about creating software that makes a difference,
+          whether it's a well-designed interface or a back-end system that runs
+          seamlessly. I'm always excited to learn new technologies and explore
+          how they can be applied to solve real-world problems.Outside of
+          building softwares, I’m always seeking new adventures. Whether it’s
+          traveling to new places or trying out different activities, I believe
+          in “remembering to live.” These experiences inspire my work and keep
+          me grounded, giving me fresh perspectives and the energy to approach
+          challenges with an open mind. I thrive on creativity, exploration, and
+          making connections that inspire growth both in and out of the tech
+          world.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
