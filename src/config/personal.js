c6a7d29ebaf0b952d062f4e7cd1402f4ca3bed74
CodieMisiani <nimrodmisiani1@gmail.com>
2025-10-14 17:17:44 +0300

    feat: add personalInfo config file with name, titles, bio, social links, and profile picture

---
+++ src/config/personal.js
@@ -0,0 +1,17 @@
+export const personalInfo = {
+  name: "Nimrod Misiani",
+  titles: [
+    "Software Engineer",
+    "Front-end Engineer",
+    "Back-end Engineer",
+    "@Codie Misiani",
+    "Problem Solver",
+  ],
+  bio: `I'm a creative problem solver...`,
+  social: {
+    linkedin: "https://www.linkedin.com/in/nimrod-misiani/",
+    github: "https://github.com/CodieMisiani",
+    email: "nimrodmisiani42@gmail.com",
+  },
+  profilePic: "/favicon2.jpeg",
+};
